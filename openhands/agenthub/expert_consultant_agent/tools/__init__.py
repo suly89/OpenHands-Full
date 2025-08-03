7fd441e78317e836029d58cd2fbf519b706d2de1
Jesus <j.martin.informatica@gmail.com>
2025-08-03 20:42:40 +0200

    refactor: Update RDTeamAgent to handle multi-action responses and improve backlog task management

---
+++ openhands/agenthub/expert_consultant_agent/tools/__init__.py
@@ -0,0 +1,7 @@
+from openhands.agenthub.expert_consultant_agent.tools.generate_mermaid import GenerateMermaidDiagramTool
+from openhands.agenthub.expert_consultant_agent.tools.validate_docs import DocumentationValidatorTool
+
+__all__ = [
+    'GenerateMermaidDiagramTool',
+    'DocumentationValidatorTool'
+]
